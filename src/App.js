140c90f1c4d8d61f9c0b38079ca84ec758e69250
Henri PHOTHINANTHA <phothinh01@gmail.com>
2022-09-23 11:43:24 +0200

    WIP

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
+import React from "react";
+import {
+  Link,
+  BrowserRouter as Router,
+  Routes,
+  Route  
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/home">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route exact path='/home' component={Home} ></Route>
+        </Routes>
+      
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  <div>return <h2>Home</h2></div>;
 }
 
 export default App;
